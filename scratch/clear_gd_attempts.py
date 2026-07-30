c7892912edf416225a6a8de5d19908efef04a9b4
veerakumar978 <23551a4491veerakumar@gmail.com>
2026-07-30 11:37:37 +0530

    feat: add clear_gd_attempts.py script

---
+++ scratch/clear_gd_attempts.py
@@ -0,0 +1,10 @@
+import pymongo
+
+remote_uri = "mongodb+srv://allampallivinaya_db_user:6lbDyU6GocG8JxLY@cluster0.l1t116x.mongodb.net/kr_academy?retryWrites=true&w=majority&appName=Cluster0"
+client = pymongo.MongoClient(remote_uri)
+db = client["kr_academy"]
+attempts_col = db["attempts"]
+
+query = {"testName": {"$regex": "(GD|Group D)", "$options": "i"}}
+res_remote = attempts_col.delete_many(query)
+print(f"Deleted {res_remote.deleted_count} attempts matching {query} in remote database.")
